43ff74ae703d17ff746a901b4e427239be024951
saraoualha <sara.oualha@ensi-uma.tn>
2021-07-21 18:12:18 +0100

    commit

---
+++ src/actions/index.js
@@ -0,0 +1,41 @@
+import _ from 'lodash';
+import jasonPlaceholder from '../apis/jsonPlaceholder'
+
+export const fetchPostsAndUsers=()=> async (dispatch,getState) =>{
+	await dispatch(fetchPosts());
+	
+	/*const userIds=_.uniq(_.map(getState().posts,'userId'));
+	userIds.forEach(id=>dispatch(fetchUser(id)));*/
+
+	_.chain(getState().posts)
+	  .map('userId')
+	  .uniq()
+	  .forEach(id => dispatch(fetchUser(id)))
+	  .value();	
+};
+
+export const fetchPosts=()=> async (dispatch)=>
+{
+	const response = await jasonPlaceholder.get('/posts');
+
+	dispatch({type:'FETCH_POSTS', payload: response.data})
+};
+
+/*export const fetchUser=(id)=> (dispatch)=>{
+	_fetchUser(id,dispatch);
+};
+
+const _fetchUser =_.memoize(async  (id,dispatch)=>{
+	const response = await jasonPlaceholder.get(`/users/${id}`);
+
+	dispatch({type: 'FETCH_USER', payload: response.data});
+});*/ //memoized version
+
+export const fetchUser=(id)=> async(dispatch)=>{
+
+
+	const response = await jasonPlaceholder.get(`/users/${id}`);
+
+	dispatch({type: 'FETCH_USER', payload: response.data});
+};
+
